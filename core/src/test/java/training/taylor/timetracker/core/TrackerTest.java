45251b5b9ea49e69819ee3cd24ed23c97d4d76f3
Ravi Desai <86118956+Ravidesa@users.noreply.github.com>
2025-08-11 19:38:17 +0530

    Update TrackerTest.java

--- core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
+++ core/src/test/java/training/taylor/timetracker/core/TrackerTest.java
@@ -3,6 +3,7 @@ package training.taylor.timetracker.core;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
